6c6ff146e5aa6160984dc0a10848437adea758d0
Kenichi Takahama <hammer528@msn.com>
2022-11-29 15:49:00 +0900

    Project7_part1「Sharing SwiftUI state with @StateObject」finish

--- Project7_part1/ContentView.swift
+++ Project7_part1/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+//ユーザーのを姓名格納する構造体  classに変更
+//classに変えると@Stateが監視できず機能しない。。
+//classを監視しているビューに通知して読み込めるようにする@publushed
+//user.classを次のように変更します
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    //ユーザープロパティの作成 userプロパティの変更
+    @StateObject var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+            
         }
-        .padding()
+
     }
 }
 
